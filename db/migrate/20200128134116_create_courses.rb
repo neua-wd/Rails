db16091c329dd4e8c21841b7c8059983e8e2745e
Worabodee <neua.worabodee@gmail.com>
2020-01-28 20:41:55 +0700

    fixed wrong user id column name for courses

--- db/migrate/20200128134116_create_courses.rb
+++ db/migrate/20200128134116_create_courses.rb
@@ -3,7 +3,7 @@ class CreateCourses < ActiveRecord::Migration[6.0]
     create_table :courses do |t|
       t.string :title
       t.string :description
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
