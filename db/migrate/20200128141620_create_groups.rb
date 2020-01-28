e2819494044dbbb00c9986e6610f4086ed882d43
Worabodee <neua.worabodee@gmail.com>
2020-01-28 21:16:45 +0700

    generated groups scaffold

---
+++ db/migrate/20200128141620_create_groups.rb
@@ -0,0 +1,12 @@
+class CreateGroups < ActiveRecord::Migration[6.0]
+  def change
+    create_table :groups do |t|
+      t.string :name
+      t.boolean :project_status
+      t.integer :score
+      t.references :course, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_134116) do
+ActiveRecord::Schema.define(version: 2020_01_28_141620) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2020_01_28_134116) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.boolean "project_status"
+    t.integer "score"
+    t.integer "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_groups_on_course_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_01_28_134116) do
   end
 
   add_foreign_key "courses", "users"
+  add_foreign_key "groups", "courses"
 end
